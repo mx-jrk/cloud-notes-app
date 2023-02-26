ff9af8214860b41c0299e8fd5cbc2f60adef5394
mx_jrk <max.yurkov@gmail.com>
2023-02-26 16:07:03 +0300

    Saving note in local memory added. Loading & Deleting note from local memory added

--- NotePage.xaml.cs
+++ NotePage.xaml.cs
@@ -2,18 +2,32 @@ namespace Cloud_Notes_App;
 
 public partial class NotePage : ContentPage
 {
-	public NotePage()
+    string fileName = Path.Combine(FileSystem.AppDataDirectory, "note.txt");
+    public NotePage()
 	{
 		InitializeComponent();
-	}
+        if (File.Exists(fileName))
+        {
+            string[] file_content = File.ReadAllText(fileName).Split('\n');
 
+            this.FindByName<Editor>("TitleEditor").Text = file_content[0];
+            this.FindByName<Editor>("TextEditor").Text = file_content[1];
+        }
+            
+    }
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
-
+        File.WriteAllText(fileName, this.FindByName<Editor>("TitleEditor").Text.ToString() + '\n' + this.FindByName<Editor>("TextEditor").Text.ToString());
     }
 
     private void DeleteButton_Clicked(object sender, EventArgs e)
     {
+        if (File.Exists(fileName))
+            File.Delete(fileName);
+
+        this.FindByName<Editor>("TitleEditor").Text= string.Empty;
+        this.FindByName<Editor>("TextEditor").Text = string.Empty;
 
     }
+
 }

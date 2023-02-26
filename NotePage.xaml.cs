fd3955713641de321b75c163d27fcc50edd89d35
mx_jrk <max.yurkov@gmail.com>
2023-02-26 12:03:09 +0300

    default color changet to orange; NotePage Layout created

---
+++ NotePage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Cloud_Notes_App;
+
+public partial class NotePage : ContentPage
+{
+	public NotePage()
+	{
+		InitializeComponent();
+	}
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
